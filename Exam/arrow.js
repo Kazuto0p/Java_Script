e8b146af1fe59ea3aa0737444a179cff24946006
Kazuto0p <122356200+Kazuto0p@users.noreply.github.com>
2025-02-08 17:47:13 +0530

    Add files via upload

---
+++ Exam/arrow.js
@@ -0,0 +1,7 @@
+const square = (num) => {
+    let a = num * num;
+    console.log(a);
+};
+
+let num = 5;
+square(num); 
---
+++ Exam/leapy.js
@@ -0,0 +1,7 @@
+function LeapYear(year) {
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+      return true;
+    }
+    return false;
+  }
+  console.log(LeapYear(2024));
